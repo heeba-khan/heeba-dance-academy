59f841a9af50f73c5f5390a17f62bb3ba7df794a
heeba-khan <heebak45@gmail.com>
2022-09-12 01:25:29 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express=require("express");
+const path=require("path");
+const app= express();
+const port=8000;
+
+//EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static'))//for serving static files
+app.use(express.urlencoded())
+
+//PUG SPECIFIC STUFF
+app.set('view engine','pug')//set the template engine as pug
+app.set('views', path.join(__dirname,'views'))//set the views directory
+
+//END POINTS
+app.get('/',(req,res)=>{
+    const con ="This is the best content on internet so far so  use it wisely";
+    const params={}
+    res.status(200).render('home.pug', params)
+
+})
+
+app.get('/contact',(req,res)=>{
+    const con ="This is the best content on internet so far so  use it wisely";
+    const params={}
+    res.status(200).render('contact.pug', params)
+
+})
+
+//START THE SERVER
+app.listen(port,()=>{
+    console.log(`The application started successfully on port ${port}`)
+})
+
